3c53185f19bf93fb9f4030a3b2b58eabbe95fa4a
DrKim <acebunny00@gmail.com>
2024-03-23 19:38:55 +0900

    🔧 use NuxtLayout
    https://nuxt.com/docs/guide/directory-structure/layouts
    https://nuxt.com/docs/api/components/nuxt-layout
    https://nuxt.com/docs/guide/directory-structure/pages
    https://nuxt.com/docs/api/components/nuxt-page
    https://nuxt.com/docs/getting-started/routing

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<div>[ {{ NODE_ENV }} ] Current route: {{ route.path }}</div>
+		<hr />
+		<slot />
+	</div>
+</template>
+
+<script setup>
+	const route = useRoute();
+	const NODE_ENV = process.env.NODE_ENV;
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
 <template>
-	<div>
-		<div>[ {{ NODE_ENV }} ] Current route: {{ route.path }}</div>
-		<hr />
-	</div>
+	<div></div>
 </template>
-
-<script setup>
-	const route = useRoute();
-	const NODE_ENV = process.env.NODE_ENV;
-</script>
